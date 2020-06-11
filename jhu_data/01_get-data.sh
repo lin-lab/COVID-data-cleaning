64c4074662a793418cd8b15a501e4d4bb10bbe3d
Andy Shi <andyshi@g.harvard.edu>
2020-06-11 15:08:43 -0400

    add JHU github as submodule: modify my code
    Former-commit-id: 37a5f3b6c33b3e5105d99ebfeac6eb612c6816fd

--- jhu_data/01_get-data.sh
+++ jhu_data/01_get-data.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-GIT_URL="https://github.com/CSSEGISandData/COVID-19/archive/master.tar.gz"
-OUT_DIR="JHU_CSSE_COVID-19"
-
-if [ ! -d ${OUT_DIR} ]
-then
-    mkdir ${OUT_DIR}
-fi
-
-echo "Downloading data from ${GIT_URL}..."
-wget --quiet --output-document=/dev/stdout ${GIT_URL} |
-    tar -xzvf /dev/stdin -C ${OUT_DIR} --strip-components=1
+git submodule update --remote JHU_CSSE_COVID-19
