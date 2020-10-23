8a51fd3b63e35274ae481e3d6c3428dd4c09a97f
Andy Shi <andyshi@g.harvard.edu>
2020-10-23 16:40:27 -0400

    switch to AWS for hosting generated files
    Former-commit-id: 7a7669bb2c7ebf175133447d6e9228912f931e98

--- jhu_data/run_jhu_cleaning.sh
+++ jhu_data/run_jhu_cleaning.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 ./01_get-data.sh && Rscript --vanilla ./02_munge-data.R
+
+cd cleaned_data
+for ext in County State Global Subnational
+do
+    filename=JHU_COVID-19_${ext}.csv
+    out_zip=${filename}.zip
+    zip - ${filename} > ${out_zip}
+    aws s3 cp ${out_zip} s3://hsph-covid-study/JHU_Cleaned/
+done
+
